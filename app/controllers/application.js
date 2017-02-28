bacf2fbcf08664606cc8e25a2bf235e601ce0839
bruno.giammella@sciensa.com <bruno.giammella@sciensa.com>
2017-02-28 15:45:43 -0300

    changing status

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  chefs: [ {name: 'Gordon Ramsay', avability: true}, 
-           {name: 'Anthony Bourdaik', avability: false}, 
-           {name: 'Rachael Ray', avability: true}, 
+  chefs: [ {name: 'Gordon Ramsay', avability: true},
+           {name: 'Anthony Bourdaik', avability: false},
+           {name: 'Rachael Ray', avability: true},
            {name: 'Jamie Oliver', avability: false},
-           {name: 'Guy Fieri', avability: true}]
+           {name: 'Guy Fieri', avability: true}],
+  actions: {
+    exit: function(chef){
+      Ember.set(chef, 'avability', false);
+    },
+    enter: function(chef){
+      Ember.set(chef, 'avability', true);
+    }
+  }
 });
